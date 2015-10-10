0de08b2f2447ec82ae8db91f588509d75990b7e0
Moore-Brendon <brendonmoore@gmail.com>
2015-10-10 13:19:01 -0500

    6 Classes added by Brendon

--- JosephofEgypt/src/josephofegypt/JosephofEgypt.java
+++ JosephofEgypt/src/josephofegypt/JosephofEgypt.java
@@ -5,8 +5,13 @@
  */
 package josephofegypt;
 
+import byui.cit260.josephInEgypt.model.Barrel;
 import byui.cit260.josephInEgypt.model.InventoryItem;
+import byui.cit260.josephInEgypt.model.MountainScene;
 import byui.cit260.josephInEgypt.model.Player;
+import byui.cit260.josephInEgypt.model.Pyramid;
+import byui.cit260.josephInEgypt.model.ResourceScene;
+import byui.cit260.josephInEgypt.model.Wagon;
 
 /**
  *
@@ -35,7 +40,51 @@ public class JosephofEgypt {
         System.out.println(inventoryInfo);
         
         
+        Pyramid pyramidOne = new Pyramid();
+        pyramidOne.setLength(25);
+        pyramidOne.setWidth(25);
+        pyramidOne.setHeight(20);
+        pyramidOne.setVolume(450);
         
+        String pyramidInfo = pyramidOne.toString();
+        System.out.println(pyramidInfo);
+     
+        
+        Barrel barrelOne = new Barrel();
+        barrelOne.setDiameter(5);
+        barrelOne.setHeight(15);
+        barrelOne.setVolume(65.4);
+        
+        String barrelInfo = barrelOne.toString();
+        System.out.println(barrelInfo);
+        
+        
+        Wagon wagonOne = new Wagon();
+        wagonOne.setMaxWeight(2000);
+        wagonOne.setAmountLoaded(500);
+        wagonOne.setLoadedWeight(1800);
+        
+        String wagonInfo = wagonOne.toString();
+        System.out.println(wagonInfo);
+        
+        ResourceScene rSceneOne = new ResourceScene();
+        rSceneOne.setDescription("Iron");
+        rSceneOne.setAmountAvailable(45);
+        rSceneOne.setResourceType("Metals");
+        rSceneOne.setTravelTime(20);
+        
+        String rSceneInfo = rSceneOne.toString();
+        System.out.println(rSceneInfo);
+        
+        MountainScene mSceneOne = new MountainScene();
+        mSceneOne.setDescription("The Great Plains");
+        mSceneOne.setHint("Here is a hint.");
+        mSceneOne.setTravelTime(23);
+        mSceneOne.setSymbol("Wheat");
+        mSceneOne.setRequiredTime(12);
+        
+        String mSceneInfo = mSceneOne.toString();
+        System.out.println(mSceneInfo);
         
         
     }
