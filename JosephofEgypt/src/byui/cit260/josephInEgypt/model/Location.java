ef6a53076052f549639d5a24071876a42791039a
Moore-Brendon <brendonmoore@gmail.com>
2015-10-21 20:21:52 -0500

    6 Classes added by Brendon

---
+++ JosephofEgypt/src/byui/cit260/josephInEgypt/model/Location.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.josephInEgypt.model;
+
+/**
+ *
+ * @author User
+ */
+public class Location {
+    private int column;
+    private int row;
+
+    public Location() {
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.column;
+        hash = 97 * hash + this.row;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.column != other.column) {
+            return false;
+        }
+        if (this.row != other.row) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "column=" + column + ", row=" + row + '}';
+    }
+    
+    
+            
+    
+}
