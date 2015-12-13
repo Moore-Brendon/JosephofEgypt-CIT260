b8f73fb6f6e9a12cdf297f98a3ed1417b72c8db5
User <User@192.168.1.67>
2015-12-12 22:09:30 -0600

    MoveMenuview dad's work

--- JosephofEgypt/src/byui/cit260/josephInEgypt/view/MoveMenuView.java
+++ JosephofEgypt/src/byui/cit260/josephInEgypt/view/MoveMenuView.java
@@ -3,121 +3,133 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package byui.cit260.josephInEgypt.view;
 
 
 import byui.cit260.josephInEgypt.control.GameControl;
 import byui.cit260.josephInEgypt.model.GameMenuView;
 import java.util.Scanner;
+//public class Josematrizjmv {
 
-/**
- *
- * @author Brendon.Moore
+    /**
+     * @param args the command line arguments
+     */
+    
+
+//public class MoveMenuView extends View {
+  
+     
+    
+// TODO code application logic here
+        
+//}
+
+//movemenuview and mapcontrol and viegamemap
+
+ /*
+ /* @author Brendon.Moore
  */
 public class MoveMenuView extends View {
     public MoveMenuView (){
-        super("\n"
-            + "\n----------------------------"
-            + "\n| Select destination territory              |"
-            + "\n----------------------------"
-            + "\nR - Riverside"
-            + "\nM - Mineral"
-            + "\nC - City"
-            + "\nV - Village"
-            + "\nT - Cattle"
-            + "\nW - Warehouse"
-            + "\nN - Mountain"
-            + "\nX - Exit"
-            + "\n----------------------------");
+        super("There are several places that you need to go"
+                + "\nfirst, you must go to every one, so"
+                + "\nplease enter two numbers; if you are in a MISSING PLACE"
+                + "\nyou need enter other place. There are 12 places");
           
             
     }
    
-    
-   
+    @Override
+   public String getInput(){
+       return null;
+   }
      @Override
     public boolean doAction(Object obj) {
-        String value =(String) obj;
-        value = value.toUpperCase();
-        char choice = value.charAt(0);
-        switch (choice) {
-  
-            
-            case 'R':
-                this.moveRiverside();
-                break;
-            case 'M':
-                this.moveMineral();
-                break;
-            case 'C':
-                this.moveCity();
-                break;
-            case 'V':
-                this.moveVillage();
-                break;
-            case 'T':
-                this.moveCattle();
-                break;
-            case 'W':
-                this.moveWarehouse();
-                break;
-            case 'N':
-                this.moveMountain();
-                break;
-				
-            case 'X':
-                return true;
-            default:
-                System.out.println("\n*** Invalid selection*** Try again");
-                break;
-                
-        }
-        return false;
-    }
+      // public class Josematrizjmv {
 
-    private void moveRiverside() {
-        GameControl.moveRiverside();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-	private void moveMineral() {
-        GameControl.moveMineral();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-	private void moveCity() {
-        GameControl.moveCity();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-        private void moveVillage() {
-        GameControl.moveVillage();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-	private void moveCattle() {
-        GameControl.moveCattle();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-	private void moveWarehouse() {
-        GameControl.moveWarehouse();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-	private void moveMountain() {
-        GameControl.moveMountain();
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
-    }
-	
-	
+    /**
+     * @param args the command line arguments
+     */
+   // public static void main(String[] args) {
+        
+//String Matriz1[][] = new String[2][3];
+
+String[][] Matriz1 = {
+
+
+    {"Satart","Mountain","Resource","MISSING PLACE"},
+    {"MISSING PLACE","pyramid","MISSING PLACE","Storage"},
+    {"Missing Place","Regular","Missing place","Finish"},
+};
+ 
+  //  for(int i=0; i<3; i++){
+    //    for(int j= 0; j<4; j++){
     
+      //  System.out.print(Matriz1[i][j] +"\t");
+        
+       // }
+        //System.out.println();
+//}
+   
+        
+        
+        
+        
+        
+        
+        Scanner sc =new Scanner(System.in);
+        int n = 0;
+        int m = 0;
+        int finish = 2 ;
+     boolean comprobacion = true;
+do{
+    comprobacion = true;
+    try{
+        try{   
+System.out.println("introduce the row?");
+
+ n = sc.nextInt();
+
+System.out.println("introduce the column?");
+    //int m = sc.nextInt();
+      m = sc.nextInt();
+      System.out.println("Introduce cero to finish one to continue");
+      finish = sc.nextInt();
+       System.out.print("you are in :" + Matriz1[n][m]);
     
+// }catch(java.lang.ArithmeticException e) { 
+       }catch(java.lang.ArrayIndexOutOfBoundsException e) {              
+            
+         System.out.println("sorry out of range");
+           comprobacion = false;
+        }
+        
+        }catch(java.lang.NumberFormatException e){
+        
+        
+          System.out.println("no se pueden ingresar letras");
+          comprobacion = false;
+          
+        }
+    if(finish == 1){
+        comprobacion = false;
+    }if(finish == 0){
+        comprobacion = true;
+    }
     
+
+}while (comprobacion != true);
+ 
+//    }
     
+
+return true;	
+    
+    } 
     
 }
-
-
-        
